935aa569845f9ff9476875abfc831bf572f4198a
Clênio Borges <54286827+clenioborgesx@users.noreply.github.com>
2024-01-12 17:24:56 -0300

    Atualizações
    Modificação do banco 2 onde ainda usa um arquivo externo para salvar os dados, e criação do banco3 sem salvar no arquivo para trabalhar com POO, mas ainda por terminar

--- Desafios/BancoDesafio3.py
+++ Desafios/BancoDesafio3.py
@@ -1,36 +1,15 @@
-import json
 import textwrap
 import string
 
-class ContaBancaria:
-    def __init__(self, digito_verificador):
+class Conta_Banco:
+    def __init__(self, digito_verificador, usuario):
         self.saldo = 0
         self.extrato = "Não foram realizadas movimentações.\n"
         self.limite = 500
         self.numero_saques = 0
         self.limite_saques = 5
         self.digito_verificador = digito_verificador
-
-    @classmethod
-    def from_json(cls, json_str):
-        data = json.loads(json_str)
-        conta = cls(data['digito_verificador'])
-        conta.saldo = data['saldo']
-        conta.extrato = data['extrato']
-        conta.limite = data['limite']
-        conta.numero_saques = data['numero_saques']
-        conta.limite_saques = data['limite_saques']
-        return conta
-
-    def to_json(self):
-        return json.dumps({
-            'saldo': self.saldo,
-            'extrato': self.extrato,
-            'limite': self.limite,
-            'numero_saques': self.numero_saques,
-            'limite_saques': self.limite_saques,
-            'digito_verificador': self.digito_verificador,
-        })
+        self.usuario = usuario
 
     def depositar(self, valor):
         if valor > 0:
@@ -64,10 +43,9 @@ class ContaBancaria:
         print(self.extrato)
         print(f"\nSaldo:\t\tR$ {self.saldo:.2f}")
 
-class SistemaBancario:
+class Sistema_Banco:
     def __init__(self):
         self.contas = []
-        self.usuarios = self.carregar_dados()
         self.numero_conta = 1000
 
     def efetuar_cadastro(self):
@@ -82,36 +60,30 @@ class SistemaBancario:
         data_nascimento = input("Informe a data de nascimento (dd/mm/aaaa): ")
         endereco = input("Informe o endereço (Logradouro, Número - Bairro - Cidade/Sigla estado): ")
         digito_verificador = string.ascii_uppercase.index(nome[0].upper()) + 1
-        conta_bancaria = ContaBancaria(digito_verificador)
-        agencia = "0226"  
         usuario = {
             "nome": nome,
             "data_nascimento": data_nascimento,
             "cpf": cpf,
             "endereco": endereco,
-            "conta_bancaria": conta_bancaria.to_json(),
-            "agencia": agencia,
+            "agencia": "0226",
             "numero_conta": self.numero_conta
         }
-        self.usuarios.append(usuario)
-        self.contas.append({"agencia": agencia, "numero_conta": self.numero_conta, "digito_verificador": digito_verificador, "usuario": usuario, "conta": conta_bancaria})
+        conta_bancaria = Conta_Banco(digito_verificador, usuario)
+        self.contas.append({"agencia": "0226", "numero_conta": self.numero_conta, "digito_verificador": digito_verificador, "usuario": usuario, "conta": conta_bancaria})
         self.numero_conta += 1
-        self.salvar_dados(self.usuarios)
         print("Usuário criado com sucesso!")
 
     def filtrar_usuario(self, cpf):
-        """Retorna o primeiro usuário que corresponde ao CPF fornecido."""
-        usuarios_filtrados = [usuario for usuario in self.usuarios if usuario["cpf"] == cpf]
+        usuarios_filtrados = [usuario for usuario in self.contas if usuario["usuario"]["cpf"] == cpf]
         return usuarios_filtrados[0] if usuarios_filtrados else None
 
     def acessar_conta(self):
-        """Permite ao usuário acessar sua conta."""
         cpf = input("Informe o CPF do usuário: ")
         usuario = self.filtrar_usuario(cpf)
 
         if usuario:
-            print(f"Bem-vindo, {usuario['nome']}!")
-            return usuario['conta_bancaria']
+            print(f"Bem-vindo, {usuario['usuario']['nome']}!")
+            return usuario['conta']
 
         print("Usuário não encontrado.")
         return None
@@ -128,38 +100,33 @@ class SistemaBancario:
                 Titular:\t{conta['usuario']['nome']}
             """
             print(textwrap.dedent(linha))
-
-    def atualizar_usuario(self, conta):
-        for usuario in self.usuarios:
-            if usuario['conta_bancaria'].digito_verificador == conta.digito_verificador:
-                usuario['conta_bancaria'] = conta
-                break
-
-
-    def salvar_dados(self, usuarios, nome_arquivo='usuarios.txt'):
-        try:
-            with open(nome_arquivo, 'w') as arquivo:
-                json.dump(usuarios, arquivo)
-        except IOError:
-            print("Erro ao salvar os dados dos usuários.")
-
-    def carregar_dados(self, nome_arquivo='usuarios.txt'):
-        try:
-            with open(nome_arquivo, 'r') as arquivo:
-                if arquivo.read().strip():  # Verifica se o arquivo não está vazio
-                    arquivo.seek(0)  # Volta ao início do arquivo
-                    usuarios = json.load(arquivo)
-                    for usuario in usuarios:
-                        usuario['conta_bancaria'] = ContaBancaria.from_json(usuario['conta_bancaria'])
-                        self.contas.append({"agencia": usuario['agencia'], "numero_conta": usuario['numero_conta'], "digito_verificador": usuario['conta_bancaria'].digito_verificador, "usuario": usuario, "conta": usuario['conta_bancaria']})
+            
+    def transferir(self, conta_origem):
+        numero_conta_destino = int(input("Digite o número da conta para a qual deseja transferir: "))
+        valor = float(input("Digite o valor que deseja transferir: "))
+        
+        for conta in self.contas:
+            if conta['numero_conta'] == numero_conta_destino:
+                print(f"Você está transferindo para {conta['usuario']['nome']}")
+                confirmacao = int(input("Digite 1 para confirmar a transferência, 2 para cancelar: "))
+                if confirmacao == 1:
+                    if conta_origem.saldo >= valor:
+                        conta_origem.saldo -= valor
+                        conta['conta'].saldo += valor
+                        conta_origem.extrato += f"Transferência para {conta['usuario']['nome']}:\tR$ {valor:.2f}\n"
+                        conta['conta'].extrato += f"Transferência de {conta_origem.usuario['nome']}:\tR$ {valor:.2f}\n"
+                        print("Transferência realizada com sucesso!")
+                    else:
+                        print("Saldo insuficiente para realizar a transferência.")
+                elif confirmacao == 2:
+                    print("Transferência cancelada.")
                 else:
-                    usuarios = []
-        except (FileNotFoundError, IOError):
-            usuarios = []
-        return usuarios
+                    print("Opção inválida.")
+                return
+        print("Conta destino não encontrada.")
 
 def menu_principal():
-    sistema = SistemaBancario()
+    sistema = Sistema_Banco()
 
     while True:
         print("\n1 - Acessar Conta")
@@ -176,7 +143,7 @@ def menu_principal():
         elif opcao == 3:
             sistema.listar_contas()
         elif opcao == 4:
-            print("Saindo do sistema...")
+            print("Até Logo!")
             break
         else:
             print("Opção inválida!")
@@ -186,21 +153,20 @@ def menu_conta(conta, sistema):
         print("\n1 - Depósito")
         print("2 - Saque")
         print("3 - Extrato")
-        print("4 - Sair")
+        print("4 - Transferência")
+        print("5 - Sair")
         opcao = int(input("Escolha uma opção: "))
         if opcao == 1:
             valor = float(input("Digite o valor do depósito: "))
             conta.depositar(valor)
-            sistema.atualizar_usuario(conta)
-            sistema.salvar_dados(sistema.usuarios)
         elif opcao == 2:
             valor = float(input("Digite o valor do saque: "))
-            conta.sacar(valor=valor)
-            sistema.atualizar_usuario(conta)
-            sistema.salvar_dados(sistema.usuarios)
+            conta.sacar(valor)
         elif opcao == 3:
             conta.exibir_extrato()
         elif opcao == 4:
+            sistema.transferir(conta)
+        elif opcao == 5:
             print("Saindo da conta...")
             break
         else:
